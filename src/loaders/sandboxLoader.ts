6d6b5a26caeae457bf5088ca312d995b2a5eafb8
Jari Leminaho <jaripiilola@gmail.com>
2024-10-07 16:34:42 +0200

    Adds nothing

--- src/loaders/sandboxLoader.ts
+++ src/loaders/sandboxLoader.ts
@@ -1,4 +1,4 @@
-import { customFetch } from '../api';
+import { adsFetch } from '../apis/api';
 
 const url = '/search?q=';
 
@@ -8,17 +8,46 @@ const url = '/search?q=';
 //   return adsResponse.hits;
 // };
 
-export const loader = async () => {
-  try {
-    const response = await customFetch.get(url);
-    const { total, hits } = response.data;
+// export const loader = async () => {
+//   try {
+//     const response = await customFetch.get(url);
+//     const { total, hits } = response.data;
 
-    return {
-      total: total.value,
-      ads: hits,
-    };
-  } catch (error) {
-    console.error('Error fetching ads data:', error);
-    throw new Response('Failed to load ads', { status: 500 });
-  }
+//     return {
+//       total: total.value,
+//       ads: hits,
+//     };
+//   } catch (error) {
+//     console.error('Error fetching ads data:', error);
+//     throw new Response('Failed to load ads', { status: 500 });
+//   }
+// };
+
+const adsQuery = (queryParams) => {
+  const { search, headline } = queryParams;
+
+  return {
+    queryKey: ['ads', search, headline],
+    queryFn: () => adsFetch.get(url, { params: { q: search, headline } }),
+  };
 };
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await queryClient.ensureQueryData(adsQuery(params));
+      const { total, hits } = response.data;
+
+      return {
+        total: total.value,
+        ads: hits,
+      };
+    } catch (error) {
+      console.error('Error fetching ads data:', error);
+      throw new Response('Failed to load ads', { status: 500 });
+    }
+  };
